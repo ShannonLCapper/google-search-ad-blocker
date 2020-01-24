a5563848229b003e340e3f7e3d9020029102f4fd
Shannon Capper <shannon@textio.com>
2020-01-23 23:11:04 -0800

    Fix issue where animation sometimes doesn't occur
    We need the browser to repaint with the max height before we set the max height to 0, if we want it to animate. By using another requestAnimationFrame, we ensure that the initial max height gets set before a repaint

--- src/content/index.js
+++ src/content/index.js
@@ -2,12 +2,14 @@ function hideAds() {
     const ads = Array.from(document.querySelectorAll(`[aria-label='Ads']`));
     const adData = ads.map(ad => ({ element: ad, height: ad.clientHeight }));
     adData.forEach(({ element, height }) => {
-        element.style.maxHeight = `${height}px`;
-        element.style.transition = `max-height 0.3s ease-out, opacity 0.3s ease-out`;
-        // Wait for the maxHeight styles to take effect, then animate to 0 with a fade
         requestAnimationFrame(() => {
-            element.style.opacity = `0`;
-            element.style.maxHeight = `0`;
+            element.style.maxHeight = `${height}px`;
+            element.style.transition = `max-height 0.3s ease-out, opacity 0.3s ease-out`;
+            // Wait for the maxHeight styles to take effect, then animate to 0 with a fade
+            requestAnimationFrame(() => {
+                element.style.opacity = `0`;
+                element.style.maxHeight = `0`;
+            });
         });
     });
 }
